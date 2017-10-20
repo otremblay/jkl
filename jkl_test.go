78c07fde175055e30e5880e837dacf5a4309039c
Olivier Tremblay <himself@otremblay.com>
2017-10-20 19:16:24 -0400

    Fix unit tests

--- jkl_test.go
+++ jkl_test.go
@@ -1,33 +1,10 @@
 package jkl
 
 import (
-	"encoding/json"
-	"fmt"
 	"os"
 	"testing"
 	"text/template"
 )
-
-func TestUnmarshalProjects(t *testing.T) {
-	f, err := os.Open("projects.json")
-	if err != nil {
-		t.Error(err)
-	}
-	dec := json.NewDecoder(f)
-	x := struct{ Projects []Project }{}
-
-	err = dec.Decode(&x)
-	if err != nil {
-		t.Error(err)
-	}
-	for _, p := range x.Projects {
-		for _, it := range p.IssueTypes {
-			for sn, f := range it.Fields {
-				fmt.Println(it.Name, sn, f.Name, f.Required, f.Schema.Type)
-			}
-		}
-	}
-}
 
 type TestType struct {
 	Field string
